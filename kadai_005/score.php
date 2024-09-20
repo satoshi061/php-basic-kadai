cd957ba0f563ef6a70305d0fdf292aa3f0166940
satoshi061 <shankou0302@gmail.com>
2024-09-20 20:48:39 +0900

    Add files via upload

---
+++ kadai_005/score.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<head>
+  <meta charset='utf-8'>
+  <title>5章課題</title>
+</head>
+<body>
+  <p>
+    <?php
+      $score1 = 80;
+      $score2 = 60;
+      $score3 = 55;
+      $score4 = 40;
+      $score5 = 100;
+      $score6 = 25;
+      $score7 = 80;
+      $score8 = 95;
+      $score9 = 30;
+      $score10 = 60;
+      $score_ave = ($score1 + $score2 + $score3 + $score4 + $score5 + $score6 + $score7 + $score8 + $score9 +$score10) / 10;
+      echo $score_ave;
+    ?>
+  </p>
+</body>
+</html>
