f2bbd4b15da4bc873433838b4c8009fafe9340df
satoshi061 <shankou0302@gmail.com>
2024-09-21 16:23:06 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<head>
+  <meta charset="utf-8">
+  <title>PHP課題007</title>
+</head>
+<body>
+  <p>
+    <?php
+      $vegetable_data = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+      print_r($vegetable_data);
+    ?>
+  </p>
+</body>
+
+</html>
